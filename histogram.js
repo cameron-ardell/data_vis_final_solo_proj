855536ba01cb768f5cc50670f9f4640edd3cdb50
S. Ardell <Sophie@gamis-Air.home>
2017-01-18 23:28:53 -0500

    trying to fix histogram
    realized was trying to do d3.v4 version when I have d3.v3

--- histogram.js
+++ histogram.js
@@ -8,7 +8,7 @@
 var actual_dataset = both;
 var selected_attr = "Fixed Acidity";
 
-
+//holds values of specific attribute
 var hist_dataset = [];
 
 //minimum and maximum value of the specific dataset, so doing domain is easier
@@ -23,97 +23,54 @@ var max_val = attr_dict[max_type];
 var actualVal = {"both": both, "red": red, "white": white};
 
 //dimensions of histogram
-var w = 915,
+var w = 960,
 	h = 500;
 
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 30, bottom: 30, left: 30},
+    width = w - margin.left - margin.right,
+    height = h - margin.top - margin.bottom;
+
+//puts values into hist_dataset
+for(var i = 0; i < actual_dataset.length; i++){
+	var newVal = actual_dataset[i][selected_attr];
+	hist_dataset.push(newVal);
+}
+
+//set x scale since don't need formatting from histogram
+var xScale = d3.scale.linear()
+      .domain([min_val, max_val])
+      .range([0, width]);
+
+console.log(hist_dataset);
+// Generate a histogram using twenty uniformly-spaced bins.
+var data = d3.layout.histogram() //<-- takes data converts into histogram format
+    //.domain(xScale.domain())
+    .bins(xScale.ticks(20));
+    (hist_dataset); //<-- which dataset I'm using
+
+console.log(data);
+var yMax = d3.max(data, function(d){return d.y});
 console.log("hi");
-
-// // set the dimensions and margins of the graph
-// var margin = {top: 10, right: 30, bottom: 30, left: 40},
-//     width = w - margin.left - margin.right,
-//     height = h - margin.top - margin.bottom;
-
-
-// //puts values into hist_dataset
-// for(var i = 0; i < actual_dataset.length; i++){
-// 	var newVal = actual_dataset[i][selected_attr];
-// 	hist_dataset.push(newVal);
-// }
-
-// console.log("Dataset:" + hist_dataset);
-
-
-// //svg element
-// var hist_svg = d3.select("body")
-// 				//.append("svg")
-// 				.attr("class", ".display")
-// 				// .attr("width", w)
-// 				// .attr("height", h)
-// 				.attr("width", width + margin.left + margin.right)
-//     			.attr("height", height + margin.top + margin.bottom)
-//   				.append("g")
-//     			.attr("transform", 
-//           			"translate(" + margin.left + "," + margin.top + ")");
-
-// //setting scales
-// var xScale = d3.scale.linear()
-// 			.domain([min_val, max_val])
-//     		.range([0, w]);
-
-// // var bins = d3.histogram()
-// // 		    .domain(xScale.domain())
-// // 		    .thresholds(x.ticks(20))
-// // 		    (hist_dataset);
-
-// var yScale = d3.scale.linear()
-//     		.range([h, 0]);
-
-// // set the parameters for the histogram
-// var histogram = d3.histogram()
-//     .value(function(d) { return d.date; })
-//     .domain(xScale.domain())
-//     .thresholds(xScale.ticks(20));
-
-
-//  // append the svg object to the body of the page
-//  // append a 'group' element to 'svg'
-//  // moves the 'group' element to the top left margin
-//  var svg = d3.select("body").append("svg")
-//      .attr("width", width + margin.left + margin.right)
-//      .attr("height", height + margin.top + margin.bottom)
-//    	 .append("g")
-//      .attr("transform", 
-//            "translate(" + margin.left + "," + margin.top + ")");
+var yMin = d3.min(data, function(d){return d.length});
 
 
 
+//set y scale
+var yScale = d3.scale.linear()
+    .domain([0, yMax])
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
 
 
-
-//============================
-
-
-
-// chart(hist_dataset);
-
-// function chart(hist_dataset) {
-// 	hist_dataset.each(function (data)) {
-// 		var bins  = histogram(data[selected_attr]);
-// 	}
-
-// 	yScale.domain([min_val, d3.max(bins, function(d) {return d.length})]);
-
-// 	// append the bar rectangles to the svg element
-// 	svg.selectAll("rect")
-//       	.data(bins)
-//     	.enter().append("rect")
-// 	      .attr("class", "bar")
-// 	      .attr("x", 1)
-// 	      .attr("transform", function(d) {
-// 			  return "translate(" + xScale(d.x0) + "," + yScale(d.length) + ")"; })
-// 	      .attr("width", function(d) { return xScale(d.x1) - xScale(d.x0) -1 ; })
-// 	      .attr("height", function(d) { return height - yScale(d.length); });
-// }
+// var svg = d3.select("body").append("svg")
+//     .attr("width", width + margin.left + margin.right)
+//     .attr("height", height + margin.top + margin.bottom)
+//   	.append("g")
+//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
 
@@ -121,29 +78,14 @@ console.log("hi");
 
 
 
-// var bar = g.selectAll(".bar")
-// 	 .data(bins)
-// 	 .enter().append("g")
-//     .attr("class", "bar")
-//     .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
 
-// bar.append("rect")
-//     .attr("x", 1)
-//     .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
-//     .attr("height", function(d) { return height - y(d.length); });
 
-// bar.append("text")
-//     .attr("dy", ".75em")
-//     .attr("y", 6)
-//     .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
-//     .attr("text-anchor", "middle")
-//     .text(function(d) { return formatCount(d.length); });
 
-// g.append("g")
-//     .attr("class", "axis axis--x")
-//     .attr("transform", "translate(0," + height + ")")
-//     .call(d3.axisBottom(x));
 
-// var y = d3.scaleLinear()
-//     .domain([0, d3.max(bins, function(d) { return d.length; })])
-//     .range([height, 0]);
+
+
+
+
+
+
+
